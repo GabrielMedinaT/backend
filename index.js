a97dd3d8222667393c84069f20d3f5867865d52e
Gabriel <gabrielmedinatorres@hotmail.com>
2023-08-16 19:23:48 +0100

    v3

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const cajones = require("./router/ruta-cajones");
 const cosas = require("./router/ruta-cosas");
 const caja = require("./router/ruta-caja");
 const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000", 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(cors());
 app.use("/api/usuarios", usuario);
