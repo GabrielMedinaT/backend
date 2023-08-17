7a2d585e229b8b153933e7b4fc9f3cf1f7dcf6eb
Gabriel <gabrielmedinatorres@hotmail.com>
2023-08-17 15:58:38 +0100

    v1

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ mongoose
   .then(() =>
     app.listen(5000, () =>
       console.log(
-        "Conectado a la base de datos y escuchando por el puerto 10000"
+        "Conectado a la base de datos y escuchando por el puerto 5000"
       )
     )
   )
