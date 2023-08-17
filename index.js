420b5a41eeda96e8015f117b920e91566e59828e
Gabriel <gabrielmedinatorres@hotmail.com>
2023-08-17 15:32:48 +0100

    v4

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const cors = require("cors"); // Importa el paquete cors
+const port = 5000;
+app.use(express.json());
 require("dotenv").config();
 const usuario = require("./router/ruta-usuarios");
 const casa = require("./router/ruta-casa");
@@ -10,30 +11,15 @@ const armario = require("./router/ruta-armario");
 const cajones = require("./router/ruta-cajones");
 const cosas = require("./router/ruta-cosas");
 const caja = require("./router/ruta-caja");
-
-app.use(express.json());
-
-// Configura las opciones de CORS
+const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:3000", // Cambia esto al dominio de tu aplicación frontend
+  origin: "http://localhost:3000",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
   optionsSuccessStatus: 204,
 };
 
-// Aplica el middleware cors con las opciones definidas
 app.use(cors(corsOptions));
-
-// Configura las cabeceras CORS usando app.use
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Métodos HTTP permitidos
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  ); // Cabeceras permitidas
-  next();
-});
 
 app.use("/api/usuarios", usuario);
 app.use("/api/casas/", casa);
@@ -48,7 +34,7 @@ mongoose
   .then(() =>
     app.listen(5000, () =>
       console.log(
-        "Conectado a la base de datos y escuchando por el puerto 5000"
+        "Conectado a la base de datos y escuchando por el puerto 10000"
       )
     )
   )
