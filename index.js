5c2d2aefff24667089aacaf81fb96076a71dd730
Gabriel <gabrielmedinatorres@hotmail.com>
2023-08-17 15:06:45 +0100

    v33

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 204,
 };
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Permitir solicitudes desde cualquier origen
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // Métodos HTTP permitidos
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  ); // Cabeceras permitidas
+  next();
+});
 app.use(cors(corsOptions));
 app.use(express.json());
 
